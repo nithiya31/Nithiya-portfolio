e7625551fb4f68707d209fd8a8530b4211c5e9fb
NITHIYA <nithiyakrishna31@gmail.com>
2024-10-08 14:14:58 +0530

    initial commit

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "nithiyakrishna31@gmail.com"; // Replace with your email address
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
